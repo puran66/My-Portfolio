e354e68c6b4bfb54f6461d98938c971b8bab982d
Puran Vishwakarma <130432123+puran66@users.noreply.github.com>
2024-05-24 12:13:27 +0530

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,16 +10,16 @@ const Footer = () => {
         <div className="social">
           <h4>Please Follow Me</h4>
           <div className="socialNetwork">
-            <a href='https://www.linkedin.com/in/puran-vishwakarma-a64819285/' className="linkdin" rel="noreferrer">
+            <a href='https://www.instagram.com/puran__3333?igsh=d2luNDNieXE4aWEx&utm_source=qr' className="linkdin" rel="noreferrer">
               <i class="fa-brands fa-linkedin"></i>
             </a>
             <a href="https://github.com/puran66" className="gitHub" rel="noreferrer">
               <i class="fa-brands fa-github"></i>
             </a>
-            <a href="https://www.linkedin.com/in/puran-vishwakarma-a64819285/" className="instagram" rel="noreferrer">
+            <a href="https://www.instagram.com/puran__3333?igsh=d2luNDNieXE4aWEx&utm_source=qr" className="instagram" rel="noreferrer">
               <i class="fa-brands fa-instagram"></i>
             </a>
-            <a href="https://www.linkedin.com/in/puran-vishwakarma-a64819285/" className="youtube" rel="noreferrer">
+            <a href="https://www.youtube.com/channel/UCb9yrhqNkzga-_JdC-ffyEQ" className="youtube" rel="noreferrer">
               <i class="fa-brands fa-youtube"></i>
             </a>
           </div>
